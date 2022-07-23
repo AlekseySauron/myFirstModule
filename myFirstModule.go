2d46eed3f16016b1886c4c5880d753b55bce9454
Shilov Aleksey <sau0kherson@gmail.com>
2022-07-23 21:06:39 +0300

    2 варианта

--- myFirstModule.go
+++ myFirstModule.go
@@ -3,6 +3,7 @@ package myFirstModule
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -29,4 +30,34 @@ func Print1(path string) {
 	fmt.Printf("Id = %v \n", result1["id"])
 	fmt.Printf("title = %v \n", result1["title"])
 	fmt.Printf("completed = %v \n", result1["completed"])
+}
+
+func Print2(path string) {
+	resp, err := http.Get(path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("2 вариант")
+	data, err := ioutil.ReadAll(resp.Body)
+
+	type StructResult struct {
+		userId    int
+		id        int
+		title     string
+		completed bool
+	}
+
+	var result2 StructResult
+	json.Unmarshal(data, &result2)
+
+	//fmt.Println("Struct is:", result2)
+
+	fmt.Printf("userId = %v \n", result2.userId)
+	fmt.Printf("Id = %v \n", result2.id)
+	fmt.Printf("title = %v \n", result2.title)
+	fmt.Printf("completed = %v \n", result2.completed)
+
 }
